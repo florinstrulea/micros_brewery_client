b0d2f3c503da76fa1068b01fcabf569ead4c1113
Florin STRULEA <florin.strulea@neo-soft.fr>
2022-12-27 17:09:42 +0100

    initial commit

---
+++ src/main/java/beer/micros_brewery_client/MicrosBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package beer.micros_brewery_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beer/micros_brewery_client/MicrosBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package beer.micros_brewery_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
